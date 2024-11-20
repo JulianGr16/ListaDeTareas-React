31ea49e9c337485f7a6a3a952b1cd0f4059d9c0a
JulianGr16 <juliangaray.2116@gmail.com>
2024-11-20 14:51:18 -0300

    termine con partes esteticas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <body>
-      
+      <main className="contaitner-fluid d-flex justify-content-center">
+        <section className="contenedor-info-form mt-5 w-50 text-center">
+          <h2 className="text-light fw-bold mt-2">Lista de Tareas</h2>
+        </section>
+      </main>
     </body>
-  )
+  );
 }
 
-export default App
+export default App;
