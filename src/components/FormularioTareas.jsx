c161ee2d04b330a85979ce630a987a9019cb8496
JulianGr16 <juliangaray.2116@gmail.com>
2024-11-28 16:13:40 -0300

    agregamos la dependencia react-hook-form, dejamos un mensaje de error en el caso que el usuario quiera crear una tarea y el campo este vacio

--- src/components/FormularioTareas.jsx
+++ src/components/FormularioTareas.jsx
@@ -2,17 +2,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import ListaTareas from "./ListaTareas";
+import { useForm } from "react-hook-form";
 
 const FormularioTareas = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) =>{
+    console.log(data)
+  }
+
   return (
     <section className="container-fluid">
-      <Form className="">
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-          <Form.Control type="text" placeholder="escribe alguna tarea..." className="me-1"/>
+          <Form.Control type="text" placeholder="escribe alguna tarea..." className="me-1" {...register('tarea', {
+            required: "Debes ingresar una tarea antes de crear"
+          })}/>
           <Button variant="outline-primary" type="submit">
           Crear
         </Button>
         </Form.Group>
+        <Form.Text className="text-danger">
+        {errors.tarea?.message}
+      </Form.Text>
       </Form>
       <ListaTareas></ListaTareas>
     </section>
